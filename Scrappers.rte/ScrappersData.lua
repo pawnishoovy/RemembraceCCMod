076f316160b4c117b3c9d7baf8d2a9f4606b27ee
filipex2000 <filipex570@gmail.com>
2021-01-29 13:10:18 +0100

    mod data progress

--- Scrappers.rte/ScrappersData.lua
+++ Scrappers.rte/ScrappersData.lua
@@ -1,7 +1,14 @@
 ScrappersData = {} -- INIT
 ScrappersData.Module = "Scrappers.rte"
 
-ScrappersData.AlphabeticalIndexing
+function ScrappersData.IndexToPrefix(index)
+	--local iterations = math.ceil(index / 26)
+	return string.upper(string.char(96 + math.min(index, 26)))
+end
+
+
+--- index: 1,2,3
+--- prefix: A,B,C
 
 ---- Weapons
 
@@ -29,55 +36,89 @@ defaultNoise["OutdoorsSemi"] = ""
 defaultNoise["IndoorsSemi"] = ""
 defaultNoise["BigIndoorsSemi"] = ""
 
+local defaultNoiseMuffled = {}
+defaultNoise["Outdoors"] = ""
+defaultNoise["Indoors"] = ""
+defaultNoise["BigIndoors"] = ""
 
--- EXAMPLE
+defaultNoise["OutdoorsSemi"] = ""
+defaultNoise["IndoorsSemi"] = ""
+defaultNoise["BigIndoorsSemi"] = ""
+
+local defaultReflectionSmall = ""
+local defaultReflectionMedium = ""
+local defaultReflectionBig = ""
+
+--- EXAMPLE
 name = "EXAMPLE"
+ScrappersData.Ammunition[name] = {}
+--
 ScrappersData.Ammunition[name].ProjectilePresetName = "" -- The name of the particle shot upon firing
 ScrappersData.Ammunition[name].ProjectileCount = 1 -- The amout of particles spawned per shot
 ScrappersData.Ammunition[name].ProjectileSpread = 0 -- More than 0 for buckshot
 ScrappersData.Ammunition[name].ProjectileVelocity = 50 -- THe base velocity of the projectile
-
+--
 ScrappersData.Ammunition[name].CasingPresetName = "" -- The name of the casing ejected upon firing
-
+--
+--
 ScrappersData.Ammunition[name].FireSound = {}
 ScrappersData.Ammunition[name].FireSound["Add"] = "" -- Container name
 ScrappersData.Ammunition[name].FireSound["AddVariants"] = 1
+ScrappersData.Ammunition[name].FireSound["AddSemi"] = ""
+ScrappersData.Ammunition[name].FireSound["AddSemiVariants"] = 1
+--
 ScrappersData.Ammunition[name].FireSound["Bass"] = ""
 ScrappersData.Ammunition[name].FireSound["BassVariants"] = 1
-
+--
+--
+ScrappersData.Ammunition[name].FireSuppressedSound = {}
+ScrappersData.Ammunition[name].FireSuppressedSound["Add"] = ""
+ScrappersData.Ammunition[name].FireSuppressedSound["AddVariants"] = 1
+ScrappersData.Ammunition[name].FireSuppressedSound["AddSemi"] = ""
+ScrappersData.Ammunition[name].FireSuppressedSound["AddSemiVariants"] = 1
+--
+ScrappersData.Ammunition[name].FireSuppressedSound["Bass"] = ""
+ScrappersData.Ammunition[name].FireSuppressedSound["BassVariants"] = 1
+--
 ScrappersData.Ammunition[name].NoiseSound = defaultNoise -- Just set it to default noise
-
+--
+ScrappersData.Ammunition[name].ReflectionSound = defaultReflectionMedium
+--
+--
 ScrappersData.Ammunition[name].BaseNoiseVolume = 1
 ScrappersData.Ammunition[name].BaseNoisePitch = 1
-
+--
 ScrappersData.Ammunition[name].BaseNoiseSemiVolume = 1
 ScrappersData.Ammunition[name].BaseNoiseSemiPitch = 1
-]]
+--
+ScrappersData.Ammunition[name].BaseBassVolume = 1
+ScrappersData.Ammunition[name].BaseBassPitch = 1
+---
 
--- 9x18
+--- 9x18
 
--- 9x18
+--- 9x18
 
--- 22LR
+--- 22LR
 
--- 45ACP
+--- 45ACP
 
--- 46x30
+--- 46x30
 
--- 50AE
+--- 50AE
 
--- 50BMG
+--- 50BMG
 
--- 458SOCOM
+--- 458SOCOM
 
--- 545x39
+--- 545x39
 
--- 556x45
+--- 556x45
 
--- 762x39
+--- 762x39
 
--- 762x51
+--- 762x51
 
--- 762x54
+--- 762x54
 
--- 3006
+--- 3006
