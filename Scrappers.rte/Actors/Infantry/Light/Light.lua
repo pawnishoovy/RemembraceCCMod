1df1c0c1ee7b3f0b0b9df0979a5e9e2050bd20f9
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-02-21 07:25:25 +0100

    base actor sounds

--- Scrappers.rte/Actors/Infantry/Light/Light.lua
+++ Scrappers.rte/Actors/Infantry/Light/Light.lua
@@ -1,8 +1,8 @@
 
 dofile("Base.rte/Constants.lua")
 require("AI/NativeHumanAI")  --dofile("Base.rte/AI/NativeHumanAI.lua")
--- package.path = package.path .. ";Heat.rte/?.lua";
--- require("Actors/Shared/HeatAIBehaviours")
+package.path = package.path .. ";Scrappers.rte/?.lua";
+require("Actors/Infantry/Light/LightAIBehaviours")
 
 function Create(self)
 	self.AI = NativeHumanAI:Create(self)
@@ -13,7 +13,46 @@ function Create(self)
 	self.visibleInventory = false;
 	
 	-- Start modded code --
+	
+	self.RTE = "Scrappers.rte";
+	self.baseRTE = "Scrappers.rte";
+	
+	self.movementSounds = {
+	Land = CreateSoundContainer("Base Cloth Light Land", "Scrappers.rte"),
+	Jump = CreateSoundContainer("Base Cloth Light Jump", "Scrappers.rte"),
+	Crouch = CreateSoundContainer("Base Cloth Light Crouch", "Scrappers.rte"),
+	Stand = CreateSoundContainer("Base Cloth Light Stand", "Scrappers.rte"),
+	Step = CreateSoundContainer("Base Cloth Light Walk", "Scrappers.rte")};
+	
+	-- self.voiceSounds = {
+	-- Pain = CreateSoundContainer("VO Pain Heat", "Scrappers.rte")};
+	
+	self.voiceSound = CreateSoundContainer("Base Cloth Light Walk", "Scrappers.rte");
+	-- MEANINGLESS! this is just so we can do voiceSound.Pos without an if check first! it will be overwritten first actual VO play
 
+	self.altitude = 0;
+	self.wasInAir = false;
+	
+	self.moveSoundTimer = Timer();
+	self.moveSoundWalkTimer = Timer();
+	self.wasCrouching = false;
+	self.wasMoving = false;
+	
+	self.healthUpdateTimer = Timer();
+	self.oldHealth = self.Health;
+
+	-- fil jump
+	
+	-- Leg Collision Detection system
+    self.feetContact = {false, false}
+    self.feetTimers = {Timer(), Timer()}
+	self.footstepTime = 100 -- 2 Timers to avoid noise
+	
+	-- Custom Jumping
+	self.isJumping = false
+	self.jumpTimer = Timer();
+	self.jumpDelay = 500;
+	self.jumpStop = Timer();
 	
 	-- End modded code
 end
@@ -37,13 +76,19 @@ function Update(self)
 	
 	-- Start modded code--
 	
-	--self.voiceSound.Pos = self.Pos;
+	self.voiceSound.Pos = self.Pos;
 	
 	if (self:IsDead() ~= true) then
 		
+		LightAIBehaviours.handleMovement(self);
+		
+		LightAIBehaviours.handleHealth(self);
 
 	else
 	
+		LightAIBehaviours.handleHeadLoss(self);
+	
+		LightAIBehaviours.handleMovement(self);
 		
 	end
 
@@ -60,9 +105,9 @@ function Destroy(self)
 	
 	-- Start modded code --
 
-	-- if not self.ToSettle then -- we have been gibbed
-		-- self.voiceSound:Stop(-1);		
-	-- end
+	if not self.ToSettle then -- we have been gibbed
+		self.voiceSound:Stop(-1);		
+	end
 	
 	-- End modded code --
 	
---
+++ Scrappers.rte/Actors/Infantry/Light/LightAIBehaviours.lua
@@ -0,0 +1,195 @@
+LightAIBehaviours = {};
+
+-- function LightAIBehaviours.createSoundEffect(self, effectName, variations)
+	-- if effectName ~= nil then
+		-- self.soundEffect = AudioMan:PlaySound(effectName .. math.random(1, variations) .. ".ogg", self.Pos, -1, 0, 130, 1, 400, false);	
+	-- end
+-- end
+
+-- no longer needed as of pre3!
+
+function LightAIBehaviours.createVoiceSoundEffect(self, soundContainer, priority, emotion, canOverridePriority)
+	if canOverridePriority == nil then
+		canOverridePriority = false;
+	end
+	local usingPriority
+	if canOverridePriority == false then
+		usingPriority = priority - 1;
+	else
+		usingPriority = priority;
+	end
+	if self.Head and soundContainer ~= nil then
+		if self.voiceSound then
+			if self.voiceSound:IsBeingPlayed() then
+				if self.lastPriority <= usingPriority then
+					self.voiceSound:Stop();
+					self.voiceSound = soundContainer;
+					soundContainer:Play(self.Pos)
+					self.lastPriority = priority;
+					return true;
+				end
+			else
+				self.voiceSound = soundContainer;
+				soundContainer:Play(self.Pos)
+				self.lastPriority = priority;
+				return true;
+			end
+		else
+			self.voiceSound = soundContainer;
+			soundContainer:Play(self.Pos)
+			self.lastPriority = priority;
+			return true;
+		end
+	end
+end
+
+function LightAIBehaviours.handleMovement(self)
+	
+	local crouching = self.controller:IsState(Controller.BODY_CROUCH)
+	local moving = self.controller:IsState(Controller.MOVE_LEFT) or self.controller:IsState(Controller.MOVE_RIGHT);
+	
+	-- Leg Collision Detection system
+    --local i = 0
+	if self:IsPlayerControlled() then -- AI doesn't update its own foot checking when playercontrolled so we have to do it
+		if self.Vel.Y > 10 then
+			self.wasInAir = true;
+		else
+			self.wasInAir = false;
+		end
+		for i = 1, 2 do
+			--local foot = self.feet[i]
+			local foot = nil
+			--local leg = self.legs[i]
+			if i == 1 then
+				foot = self.FGFoot 
+			else
+				foot = self.BGFoot 
+			end
+
+			--if foot ~= nil and leg ~= nil and leg.ID ~= rte.NoMOID then
+			if foot ~= nil then
+				local footPos = foot.Pos				
+				local mat = nil
+				local pixelPos = footPos + Vector(0, 4)
+				self.footPixel = SceneMan:GetTerrMatter(pixelPos.X, pixelPos.Y)
+				--PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13)
+				if self.footPixel ~= 0 then
+					mat = SceneMan:GetMaterialFromID(self.footPixel)
+				--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 162);
+				--else
+				--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13);
+				end
+				
+				local movement = (self.controller:IsState(Controller.MOVE_LEFT) == true or self.controller:IsState(Controller.MOVE_RIGHT) == true or self.Vel.Magnitude > 3)
+				if mat ~= nil then
+					--PrimitiveMan:DrawTextPrimitive(footPos, mat.PresetName, true, 0);
+					if self.feetContact[i] == false then
+						self.feetContact[i] = true
+						if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then																	
+							self.feetTimers[i]:Reset()
+						end
+					end
+				else
+					if self.feetContact[i] == true then
+						self.feetContact[i] = false
+						if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then
+							self.feetTimers[i]:Reset()
+						end
+					end
+				end
+			end
+		end
+	else
+		if self.AI.flying == true and self.wasInAir == false then
+			self.wasInAir = true;
+		elseif self.AI.flying == false and self.wasInAir == true then
+			self.wasInAir = false;
+			self.isJumping = false
+			if self.moveSoundTimer:IsPastSimMS(500) then
+				self.movementSounds.Land:Play(self.Pos);
+				self.moveSoundTimer:Reset();
+			end
+		end
+	end
+	
+	
+	
+	-- Custom Jump
+	if self.controller:IsState(Controller.BODY_JUMPSTART) == true and self.controller:IsState(Controller.BODY_CROUCH) == false and self.jumpTimer:IsPastSimMS(self.jumpDelay) and not self.isJumping then
+		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) or self.wasInAir == false then
+			local jumpVec = Vector(0,-1.5)
+			local jumpWalkX = 3
+			if self.controller:IsState(Controller.MOVE_LEFT) == true then
+				jumpVec.X = -jumpWalkX
+			elseif self.controller:IsState(Controller.MOVE_RIGHT) == true then
+				jumpVec.X = jumpWalkX
+			end
+			self.movementSounds.Jump:Play(self.Pos);
+			if math.abs(self.Vel.X) > jumpWalkX * 2.0 then
+				self.Vel = Vector(self.Vel.X, self.Vel.Y + jumpVec.Y)
+			else
+				self.Vel = Vector(self.Vel.X + jumpVec.X, self.Vel.Y + jumpVec.Y)
+			end
+			self.isJumping = true
+			self.jumpTimer:Reset()
+			self.jumpStop:Reset()
+		end
+	elseif self.isJumping or self.wasInAir then
+		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) and self.jumpStop:IsPastSimMS(100) then
+			self.isJumping = false
+			self.wasInAir = false;
+			if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
+				self.movementSounds.Land:Play(self.Pos);
+				self.moveSoundTimer:Reset();
+			end
+		end
+	end
+
+	if (crouching) then
+		if (not self.wasCrouching and self.moveSoundTimer:IsPastSimMS(600)) then
+			self.movementSounds.Crouch:Play(self.Pos);
+		end
+		if (moving) then
+			if (self.moveSoundWalkTimer:IsPastSimMS(700)) then
+				self.movementSounds.Step:Play(self.Pos);
+				self.moveSoundWalkTimer:Reset();
+			end
+		end
+	else
+		if (self.wasCrouching and self.moveSoundTimer:IsPastSimMS(600)) then
+			self.movementSounds.Stand:Play(self.Pos);
+			self.moveSoundTimer:Reset();
+		end
+	end
+
+	self.wasCrouching = crouching;
+	self.wasMoving = moving;
+end
+
+function LightAIBehaviours.handleHealth(self)
+
+	local healthTimerReady = self.healthUpdateTimer:IsPastSimMS(750);
+	local wasInjured = self.Health < (self.oldHealth - 10);
+
+	if (healthTimerReady or wasInjured) then
+		self.oldHealth = self.Health;
+		self.healthUpdateTimer:Reset();
+		
+		if (wasInjured) and self.Head then
+			
+			-- if self.Health > 0 then
+				-- LightAIBehaviours.createVoiceSoundEffect(self, self.voiceSounds.Pain, 5)
+			-- else
+				-- LightAIBehaviours.createVoiceSoundEffect(self, self.voiceSounds.Death, 6)
+			-- end
+		end
+	end
+	
+end
+
+function LightAIBehaviours.handleHeadLoss(self)
+	if not (self.Head) then
+		self.voiceSounds = {};
+		self.voiceSound:Stop(-1);
+	end
+end
