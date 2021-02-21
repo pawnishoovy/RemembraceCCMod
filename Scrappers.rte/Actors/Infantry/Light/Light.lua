9b155f2349d248f3d61e67af590b9da685529785
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-02-21 11:07:45 +0100

    much better actors

--- Scrappers.rte/Actors/Infantry/Light/Light.lua
+++ Scrappers.rte/Actors/Infantry/Light/Light.lua
@@ -17,12 +17,112 @@ function Create(self)
 	self.RTE = "Scrappers.rte";
 	self.baseRTE = "Scrappers.rte";
 	
+	-- TERRAIN SOUNDS
+	
+	self.terrainSounds = {
+	Crawl = {[12] = CreateSoundContainer("Crawl Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("Crawl Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("Crawl Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("Crawl Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("Crawl Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("Crawl Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("Crawl Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("Crawl Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("Crawl SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("Crawl SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("Crawl SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("Crawl SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("Crawl SolidMetal", "Scrappers.rte")},
+	Prone = {[12] = CreateSoundContainer("Prone Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("Prone Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("Prone Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("Prone Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("Prone Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("Prone Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("Prone Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("Prone Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("Prone SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("Prone SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("Prone SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("Prone SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("Prone SolidMetal", "Scrappers.rte")},
+	TerrainImpactLight = {[12] = CreateSoundContainer("TerrainImpact Light Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("TerrainImpact Light Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("TerrainImpact Light Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("TerrainImpact Light Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("TerrainImpact Light Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("TerrainImpact Light Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("TerrainImpact Light Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("TerrainImpact Light Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("TerrainImpact Light SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("TerrainImpact Light SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("TerrainImpact Light SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("TerrainImpact Light SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("TerrainImpact Light SolidMetal", "Scrappers.rte")},
+	TerrainImpactHeavy = {[12] = CreateSoundContainer("TerrainImpact Heavy Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("TerrainImpact Heavy Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("TerrainImpact Heavy Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("TerrainImpact Heavy Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("TerrainImpact Heavy Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("TerrainImpact Heavy Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("TerrainImpact Heavy Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("TerrainImpact Heavy Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("TerrainImpact Heavy SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("TerrainImpact Heavy SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("TerrainImpact Heavy SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("TerrainImpact Heavy SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("TerrainImpact Heavy SolidMetal", "Scrappers.rte")},
+	FootstepJump = {[12] = CreateSoundContainer("Footstep Jump Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("Footstep Jump Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("Footstep Jump Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("Footstep Jump Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("Footstep Jump Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("Footstep Jump Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("Footstep Jump Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("Footstep Jump Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("Footstep Jump SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("Footstep Jump SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("Footstep Jump SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("Footstep Jump SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("Footstep Jump SolidMetal", "Scrappers.rte")},
+	FootstepLand = {[12] = CreateSoundContainer("Footstep Land Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("Footstep Land Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("Footstep Land Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("Footstep Land Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("Footstep Land Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("Footstep Land Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("Footstep Land Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("Footstep Land Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("Footstep Land SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("Footstep Land SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("Footstep Land SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("Footstep Land SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("Footstep Land SolidMetal", "Scrappers.rte")},
+	FootstepWalk = {[12] = CreateSoundContainer("Footstep Walk Concrete", "Scrappers.rte"),
+			[164] = CreateSoundContainer("Footstep Walk Concrete", "Scrappers.rte"),
+			[177] = CreateSoundContainer("Footstep Walk Concrete", "Scrappers.rte"),
+			[9] = CreateSoundContainer("Footstep Walk Dirt", "Scrappers.rte"),
+			[10] = CreateSoundContainer("Footstep Walk Dirt", "Scrappers.rte"),
+			[11] = CreateSoundContainer("Footstep Walk Dirt", "Scrappers.rte"),
+			[6] = CreateSoundContainer("Footstep Walk Sand", "Scrappers.rte"),
+			[8] = CreateSoundContainer("Footstep Walk Sand", "Scrappers.rte"),
+			[178] = CreateSoundContainer("Footstep Walk SolidMetal", "Scrappers.rte"),
+			[179] = CreateSoundContainer("Footstep Walk SolidMetal", "Scrappers.rte"),
+			[180] = CreateSoundContainer("Footstep Walk SolidMetal", "Scrappers.rte"),
+			[181] = CreateSoundContainer("Footstep Walk SolidMetal", "Scrappers.rte"),
+			[182] = CreateSoundContainer("Footstep Walk SolidMetal", "Scrappers.rte")},
+	};
+	
+	-- EVERYTHING ELSE
+	
 	self.movementSounds = {
 	Land = CreateSoundContainer("Base Cloth Light Land", "Scrappers.rte"),
 	Jump = CreateSoundContainer("Base Cloth Light Jump", "Scrappers.rte"),
 	Crouch = CreateSoundContainer("Base Cloth Light Crouch", "Scrappers.rte"),
 	Stand = CreateSoundContainer("Base Cloth Light Stand", "Scrappers.rte"),
-	Step = CreateSoundContainer("Base Cloth Light Walk", "Scrappers.rte")};
+	Step = CreateSoundContainer("Base Cloth Light Walk", "Scrappers.rte"),
+	Prone = CreateSoundContainer("Base Cloth Light Prone", "Scrappers.rte"),
+	Crawl = CreateSoundContainer("Base Cloth Light Crawl", "Scrappers.rte")};
 	
 	-- self.voiceSounds = {
 	-- Pain = CreateSoundContainer("VO Pain Heat", "Scrappers.rte")};
@@ -37,9 +137,12 @@ function Create(self)
 	self.moveSoundWalkTimer = Timer();
 	self.wasCrouching = false;
 	self.wasMoving = false;
-	
+
 	self.healthUpdateTimer = Timer();
 	self.oldHealth = self.Health;
+	
+	self.ragdollTerrainImpactTimer = Timer();
+	self.ragdollTerrainImpactDelay = math.random(200, 500);
 
 	-- fil jump
 	
@@ -54,7 +157,37 @@ function Create(self)
 	self.jumpDelay = 500;
 	self.jumpStop = Timer();
 	
+	self.lastVel = Vector(0, 0)
+	
 	-- End modded code
+end
+
+function OnCollideWithTerrain(self, terrainID)
+	
+	-- if self.impulse.Magnitude > self.ImpulseDamageThreshold then
+		-- if self.impactSoundTimer:IsPastSimMS(700) then
+			-- print("heavy")
+			-- if self.terrainSounds.TerrainImpactHeavy[terrainID] ~= nil then
+				-- self.terrainSounds.TerrainImpactHeavy[terrainID]:Play(self.Pos);
+			-- else -- default to concrete
+				-- self.terrainSounds.TerrainImpactHeavy[177]:Play(self.Pos);
+			-- end
+			-- self.impactSoundTimer:Reset();
+		-- end
+	-- elseif self.impulse.Magnitude > self.ImpulseDamageThreshold/5 then
+		-- if self.impactSoundTimer:IsPastSimMS(700) then
+			-- print("light")
+			-- if self.terrainSounds.TerrainImpactLight[terrainID] ~= nil then
+				-- self.terrainSounds.TerrainImpactLight[terrainID]:Play(self.Pos);
+			-- else -- default to concrete
+				-- self.terrainSounds.TerrainImpactLight[177]:Play(self.Pos);
+			-- end
+			-- self.impactSoundTimer:Reset();
+		-- end
+	-- end
+	
+	self.terrainCollided = true;
+	self.terrainCollidedWith = terrainID;
 end
 
 function Update(self)
@@ -76,6 +209,9 @@ function Update(self)
 	
 	-- Start modded code--
 	
+	self.impulse = (self.Vel - self.lastVel) / TimerMan.DeltaTimeSecs * self.Vel.Magnitude * 0.1
+	self.lastVel = Vector(self.Vel.X, self.Vel.Y)
+	
 	self.voiceSound.Pos = self.Pos;
 	
 	if (self:IsDead() ~= true) then
@@ -91,6 +227,15 @@ function Update(self)
 		LightAIBehaviours.handleMovement(self);
 		
 	end
+
+	if self.Status == 1 or self.Dying then
+		LightAIBehaviours.handleRagdoll(self)
+	end
+
+	-- clear terrain stuff after we did everything that used em
+	
+	self.terrainCollided = false;
+	self.terrainCollidedWith = nil;
 
 end
 -- End modded code --
--- Scrappers.rte/Actors/Infantry/Light/LightAIBehaviours.lua
+++ Scrappers.rte/Actors/Infantry/Light/LightAIBehaviours.lua
@@ -125,6 +125,17 @@ function LightAIBehaviours.handleMovement(self)
 				jumpVec.X = jumpWalkX
 			end
 			self.movementSounds.Jump:Play(self.Pos);
+			
+			local pos = Vector(0, 0);
+			SceneMan:CastObstacleRay(self.Pos, Vector(0, 45), pos, Vector(0, 0), self.ID, self.Team, 0, 10);				
+			local terrPixel = SceneMan:GetTerrMatter(pos.X, pos.Y)
+			
+			if self.terrainSounds.FootstepJump[terrPixel] ~= nil then
+				self.terrainSounds.FootstepJump[terrPixel]:Play(self.Pos);
+			else -- default to concrete
+				self.terrainSounds.FootstepJump[177]:Play(self.Pos);
+			end
+			
 			if math.abs(self.Vel.X) > jumpWalkX * 2.0 then
 				self.Vel = Vector(self.Vel.X, self.Vel.Y + jumpVec.Y)
 			else
@@ -141,18 +152,54 @@ function LightAIBehaviours.handleMovement(self)
 			if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
 				self.movementSounds.Land:Play(self.Pos);
 				self.moveSoundTimer:Reset();
+				
+				local pos = Vector(0, 0);
+				SceneMan:CastObstacleRay(self.Pos, Vector(0, 45), pos, Vector(0, 0), self.ID, self.Team, 0, 10);				
+				local terrPixel = SceneMan:GetTerrMatter(pos.X, pos.Y)
+				
+				if self.terrainSounds.FootstepLand[terrPixel] ~= nil then
+					self.terrainSounds.FootstepLand[terrPixel]:Play(self.Pos);
+				else -- default to concrete
+					self.terrainSounds.FootstepLand[177]:Play(self.Pos);
+				end						
+				
 			end
 		end
 	end
 
 	if (crouching) then
 		if (not self.wasCrouching and self.moveSoundTimer:IsPastSimMS(600)) then
-			self.movementSounds.Crouch:Play(self.Pos);
+			if (moving) then
+				self.movementSounds.Prone:Play(self.Pos);
+			else
+				self.movementSounds.Crouch:Play(self.Pos);
+			end
 		end
 		if (moving) then
+			if self.terrainCollided == true and self.proneTerrainSoundPlayed ~= true then
+				self.proneTerrainSoundPlayed = true;
+				
+				if self.terrainSounds.Prone[self.terrainCollidedWith] ~= nil then
+					self.terrainSounds.Prone[self.terrainCollidedWith]:Play(self.Pos);
+				else -- default to concrete
+					self.terrainSounds.Prone[177]:Play(self.Pos);
+				end
+			end
+				
 			if (self.moveSoundWalkTimer:IsPastSimMS(700)) then
-				self.movementSounds.Step:Play(self.Pos);
+				self.movementSounds.Crawl:Play(self.Pos);
 				self.moveSoundWalkTimer:Reset();
+				
+				local pos = Vector(0, 0);
+				SceneMan:CastObstacleRay(self.Pos, Vector(0, 20), pos, Vector(0, 0), self.ID, self.Team, 0, 5);				
+				local terrPixel = SceneMan:GetTerrMatter(pos.X, pos.Y)
+				
+				if self.terrainSounds.Crawl[terrPixel] ~= nil then
+					self.terrainSounds.Crawl[terrPixel]:Play(self.Pos);
+				else -- default to concrete
+					self.terrainSounds.Crawl[177]:Play(self.Pos);
+				end		
+				
 			end
 		end
 	else
@@ -160,6 +207,7 @@ function LightAIBehaviours.handleMovement(self)
 			self.movementSounds.Stand:Play(self.Pos);
 			self.moveSoundTimer:Reset();
 		end
+		self.proneTerrainSoundPlayed = false;
 	end
 
 	self.wasCrouching = crouching;
@@ -185,6 +233,92 @@ function LightAIBehaviours.handleHealth(self)
 		end
 	end
 	
+end
+
+function LightAIBehaviours.handleRagdoll(self)
+	-- EXPERIMENTAL BETTER RAGDOLL
+	local radius = self.Radius * 0.9
+	
+	local min_value = -math.pi;
+	local max_value = math.pi;
+	local value = self.RotAngle
+	local result;
+	local ret = 0
+	
+	local range = max_value - min_value;
+	if range <= 0 then
+		result = min_value;
+	else
+		ret = (value - min_value) % range;
+		if ret < 0 then ret = ret + range end
+		result = ret + min_value;
+	end
+	
+	local str = math.max(1 - math.abs(result / math.pi * 2.0), 0)
+	--local normal = Vector(0,0)
+	--local slide = false
+	
+	-- Trip on the ground
+	for j = 0, 1 do
+		local pos = self.Pos + Vector(0, -3):RadRotate(self.RotAngle)
+		if self.Head and math.random(1,2) < 2 then
+			pos = self.Head.Pos
+		end
+		
+		local maxi = 6
+		for i = 0, maxi do
+			local checkVec = Vector(radius * (0.7 - 0.2 * j),0):RadRotate(math.pi * 2 / maxi * i + self.RotAngle)
+			local checkOrigin = Vector(pos.X, pos.Y) + checkVec + Vector(self.Vel.X, self.Vel.Y) * rte.PxTravelledPerFrame * 0.3
+			local checkPix = SceneMan:GetTerrMatter(checkOrigin.X, checkOrigin.Y)
+			
+			if checkPix > 0 then
+				self.Vel = self.Vel - Vector(checkVec.X, checkVec.Y):SetMagnitude(30 * str) * TimerMan.DeltaTimeSecs
+				self.AngularVel = self.AngularVel + (self.AngularVel / math.abs(self.AngularVel)) * str * 20 * TimerMan.DeltaTimeSecs
+				
+				--normal = normal + checkVec
+				--slide = true
+			end
+		end
+		
+	end
+	--[[
+	normal = (normal / 12):SetMagnitude(1)
+	if slide then
+		local slideAngle = normal.AbsRadAngle + math.pi * 0.5
+		
+		local newVel = Vector(self.Vel.X, self.Vel.Y)
+		newVel:RadRotate(slideAngle)
+		newVel = Vector(newVel.X, -math.abs(-newVel.Y))
+		newVel:RadRotate(-slideAngle)
+		self.Vel = self.Vel * str + newVel * (1 - str)
+	end]]
+	-- SOUNDS
+	
+	local mat = self.HitWhatTerrMaterial
+	--PrimitiveMan:DrawLinePrimitive(self.Pos, self.Pos + self.TravelImpulse * 0.1, 5);
+	--PrimitiveMan:DrawLinePrimitive(self.Pos, self.Pos + self.Vel, 13);
+	--self.TravelImpulse.Magnitude
+	if mat ~= 0 then
+		if self.TravelImpulse.Magnitude > self.ImpulseDamageThreshold and self.ragdollTerrainImpactTimer:IsPastSimMS(self.ragdollTerrainImpactDelay) then
+			if self.terrainSounds.TerrainImpactHeavy[self.terrainCollidedWith] ~= nil then
+				self.terrainSounds.TerrainImpactHeavy[self.terrainCollidedWith]:Play(self.Pos);
+			else -- default to concrete
+				self.terrainSounds.TerrainImpactHeavy[177]:Play(self.Pos);
+			end
+			self.ragdollTerrainImpactDelay = math.random(200, 500)
+			self.ragdollTerrainImpactTimer:Reset()
+		elseif self.TravelImpulse.Magnitude > 400 and self.ragdollTerrainImpactTimer:IsPastSimMS(self.ragdollTerrainImpactDelay) then
+			if self.terrainSounds.TerrainImpactLight[self.terrainCollidedWith] ~= nil then
+				self.terrainSounds.TerrainImpactLight[self.terrainCollidedWith]:Play(self.Pos);
+			else -- default to concrete
+				self.terrainSounds.TerrainImpactLight[177]:Play(self.Pos);
+			end
+			self.ragdollTerrainImpactDelay = math.random(200, 500)
+			self.ragdollTerrainImpactTimer:Reset()
+		elseif self.TravelImpulse.Magnitude > 230 then
+			self.movementSounds.Crawl:Play(self.Pos);
+		end
+	end
 end
 
 function LightAIBehaviours.handleHeadLoss(self)
